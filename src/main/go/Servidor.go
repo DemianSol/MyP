c6ddce1c6800a7fe692e30333900f61f144cc37d
DemianSol <MAIL>
2026-09-10 23:42:58 -0600

    refactor: agrega constructor

--- src/main/go/Servidor.go
+++ src/main/go/Servidor.go
@@ -1,6 +1,7 @@
-
+:
 import (
        "net"   
+       "log"
 )
 
 
@@ -16,13 +17,32 @@ func NewServidor(puerto int) *servidor {
      server := servidor{puerto: puerto}
      server.conexiones = make(map[int]conexion)
      server.salas = make[map[string]Sala]
-     server.serverSocket = net.Listen("tcp", puerto)
      return &server
 }
 
-
+// ESTO FUE TOMADO DE GOBYEXAMPLE/TCP SERVER.COM  
 func iniciaServidor(){
-     server.conexionActiva = true 
+     server.conexionActiva = true
+     //como llamar a variables de clase
+     enviaMensajePublico("El puerto %d está disponible", puerto) 
+
+     server.serverSocket, err := net.Listen("tcp", puerto)
+     
+     if err != nil{ //// por que dice que tiene que ser distinto. REVISAR
+          log.Fatal("Error al escuchar:", err) /// debe ser de tipo mensaaje.  CAMBIAR
+     }
+     defer listener.Close()
+
+
+     for{
+          conn, err := serverSocket.Accept()
+          if err != nil{
+               log.Println("Error al aceptar la conexion", err)
+          }
+
+          go RecibeMensajes(conn, //revisar si tambien debe recibir algo mas)
+
+     }
 }
 
 func agregaConexion(){
@@ -30,9 +50,47 @@ func agregaConexion(){
 
 }
 
+func terminaConexion(conn conexion){
+
+}
+
+
+func enviaMensajePublico(){
+
+}
+
+
+func enviaMensajeUsuario(){
+
+}
+
+
+func enviaMensajeSala(){
+
+}
+
+
+
+func RecibeMensajes(conn conexion, mensaje string){
+
+     // mando a llamara recibePeticion() y recibeMensaje segun el caso
+}
+func recibePeticion(){
+
+}
+
+
+func recibeMensaje(conn conexion, mensaje string){
+
+}
+
+
 func verificaUsuario(nombreUsuario string){
 
 }
+
+
+
 
 func creaSala(nombreSala string){
 
@@ -46,9 +104,6 @@ func usuariosActivos(){
 
 }
 
-func terminaConexion(){
-
-}
 
 
 
