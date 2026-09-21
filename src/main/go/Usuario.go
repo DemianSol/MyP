b8cc44107ee3f0e2cb87dd6c4cb34199c8c76fa3
DemianSol <MAIL>
2026-09-21 04:54:53 -0600

    feat(Usuario): Añadir nuevo struct para representar clientes

---
+++ src/main/go/Usuario.go
@@ -0,0 +1,44 @@
+
+
+type Status string
+
+const (
+	ACTIVE Status = "ACTIVE"
+	BUSY Status = "BUSY"
+	AWAY Status = "AWAY"
+)
+
+type Usuario struct{
+	nombre string
+	status Status
+	conexion *Conexion
+}
+
+
+func NewUsuario(nombre string, conexion *Conexion) *Usuario{
+	u := new(Usuario)
+	u.nombre = nombre
+	u.conexion = conexion
+	u.status = ACTIVE
+	return u
+}
+
+func (u Usuario) getConexion() *Conexion{
+	return u.conexion
+}
+
+func (u Usuario) getNombre() string{
+	return u.nombre
+}
+
+func (u Usuario) getStatus() Status{
+	return u.status
+}
+
+func (u *Usuario) setStatus(status Status) error{
+	u.status = status
+	if err != nil{
+		return fmt.Errorf("Error al asignar el nuevo estatus al usuario")
+	}
+	return nil
+}
